5752e2976bb121a17d8e51218cecdfea830cd1dc
Toni & Elkana Lumbantobing <pair+chrisantoni.elkana-ltobing@devbootcamp.com>
2016-09-26 20:32:21 +0700

    toni-elkana

--- number_in_words.js
+++ number_in_words.js
@@ -1,161 +1,89 @@
-function in_words(angkaToWord){
-var result = []
-var satuanAngka = ["satu", "dua", "tiga","empat",'lima','enam','tujuh','delapan','sembilan'];
-var angkaSatuan = [1,2,3,4,5,6,7,8,9]
-var angkaInWordPuluhan = ['sepuluh','sebelas','belas','puluh']
-var angkaInWordRatusan = ['seratus','ratus']
-var angkaInWordRibuan = ['seribu','ribu']
+function in_words(n) {
+    var tampungRoman = []
+    var angka = ["","satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"]
 
+    var ratusJuta = Math.floor((n%1000000000)/100000000)
+    var puluhJuta = Math.floor((n%100000000)/10000000)
+    var jutaan = Math.floor((n%10000000)/1000000)
+    var ratusRibu = Math.floor((n%1000000)/100000)
+    var puluhRibuan = Math.floor((n%100000)/10000)
+    var ribuan = Math.floor((n%10000)/1000)
+    var ratusan = Math.floor((n%1000)/100)
+    var puluhan = Math.floor((n%100)/10)
+    var satuan = Math.floor((n%10)/1)
 
-var ratusJuta = Math.floor((angkaToWord%1000000000)/100000000)
-var puluhJuta = Math.floor((angkaToWord%100000000)/10000000)
-var jutaan = Math.floor((angkaToWord%10000000)/1000000)
-var ratusRibu = Math.floor((angkaToWord%1000000)/100000)
-var puluhRibuan = Math.floor((angkaToWord%100000)/10000)
-var ribuan = Math.floor((angkaToWord%10000)/1000)
-var ratusan = Math.floor((angkaToWord%1000)/100)
-var puluhan = Math.floor((angkaToWord%100)/10)
-var satuan = Math.floor((angkaToWord%10)/1)
-
-console.log(puluhJuta+" "+jutaan+" "+ratusRibu+" "+puluhRibuan+" "+ribuan+" "+ratusan+" "+puluhan+" "+satuan);
-
-
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if( i === puluhJuta)
-  {
-    if(puluhJuta===0)break
-    result.push(satuanAngka[i-1] + " puluh")
-  }
-}
-
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if( i === jutaan)
-  {
-    if(jutaan===0)break
-    result.push(satuanAngka[i-1] + " juta")
-  }
-}
-
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if( i === ratusRibu)
-  {
-    if(ratusRibu===0)break
-    result.push(satuanAngka[i-1] + " ratus ribu")
-  }
-}
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if( i === puluhRibuan)
-  {
-    if(puluhRibuan===0)break
-    result.push(satuanAngka[i-1] + " puluh")
-  }
-}
-
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if( i === ribuan)
-  {
-    if(ribuan===0)break
-    result.push(satuanAngka[i-1] + " ribu")
-  }
-}
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if(i === ratusan)
-  {
-    if(ratusan===0)break
-    result.push(satuanAngka[i-1]+ " ratus")
-  }
-}
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if(angkaToWord > 10 && angkaToWord <= 19)
-  {
-    if(angkaToWord===11)
-    {
-    result.push("sebelas")
-    break
-  }else if(angkaToWord > 11)
-  {
-    if(angkaToWord === 12){
-    result.push(satuanAngka[i+1]+" belas")
-    break
-  } else if(angkaToWord === 13){
-    result.push(satuanAngka[i+2]+" belas")
-    break
-  }else if(angkaToWord === 14){
-    result.push(satuanAngka[i+3]+" belas")
-    break
-  }
-  else if(angkaToWord === 15){
-    result.push(satuanAngka[i+4]+" belas")
-    break
-  }else if(angkaToWord === 16){
-    result.push(satuanAngka[i+5]+" belas")
-    break
-  }else if(angkaToWord === 17){
-    result.push(satuanAngka[i+6]+" belas")
-    break
-  }else if(angkaToWord === 18){
-    result.push(satuanAngka[i+7]+" belas")
-    break
-  }else if(angkaToWord === 19){
-    result.push(satuanAngka[i+8]+" belas")
-    break
-  }
-  }
-    if(i===satuan)
-    {
-      if(satuan===0)break
-      result.push(satuanAngka[i-1])
-   }
-  }else if(angkaToWord>=20){
-  if(i===puluhan)
-  {
-    if(puluhan===0)break
-     result.push(satuanAngka[i-1]+ " puluh")
-  }
-}
-}
-
-for(var i=0;i<satuanAngka.length+1;i++){
-  if(angkaToWord > 10 && angkaToWord <= 19){
-    if(i===puluhan)
-    {
-      if(puluhan===0)break
-       //result.push(satuanAngka[i-1]+ " puluh")
+    // push puluhan
+    if(puluhJuta>1){
+      tampungRoman.push(" " +angka[puluhJuta] + " puluh ")
+      if(jutaan>0){
+      tampungRoman.push(" " +angka[jutaan]+ " juta ")
+      }
     }
-  }else if(angkaToWord >= 20){
-   if(i===satuan)
-   {
-     if(satuan===0)break
-     result.push(satuanAngka[i-1])
-  }
-}
-else if(angkaToWord >= 1 && angkaToWord < 10){
- if(i===satuan)
- {
-   if(satuan===0)break
-   result.push(satuanAngka[i-1])
-}
-}else if(angkaToWord === 10)
-{
-  result.push("sepuluh")
-  break
-}
-}
-console.log(result.join(' '));
+    else if(puluhJuta==1){
+      if(jutaan>1){
+      tampungRoman.push(" "+angka[jutaan] + " belas juta ")
+      }
+      else if(jutaan==1){
+      tampungRoman.push(" "+"sebelas juta ")
+      }
+
+    }
+    // push puluhRibuan
+    else if(puluhRibuan==0){
+      tampungRoman.push(angka[jutaan])
+    }
+
+    // push ratusRibuan
+    if(ratusRibu==1){
+      tampungRoman.push("seratus ")
+    }
+    else if(ratusRibu>0){
+    tampungRoman.push(angka[ratusRibu] + " ratus ")
+    }
+
+    // push puluhRibuan
+    if(puluhRibuan>0){
+    tampungRoman.push(angka[puluhRibuan] + " puluh ")
+    }
+    // push ribuan
+      if(ribuan>0){
+      tampungRoman.push(angka[ribuan] + " ribu")
+      }
+    // push ratusan
+    if(ratusan==1){
+      tampungRoman.push("seratus ")
+    }
+
+    else if(ratusan>0){
+      tampungRoman.push(" " +angka[ratusan] + " ratus")
+    }
+    // push puluhan
+    if(puluhan>1){
+      tampungRoman.push(" " +angka[puluhan] + " puluh")
+      if(satuan>0){
+      tampungRoman.push(" " +angka[satuan])
+      }
+    }
+    else if(puluhan==1){
+      if(satuan>1){
+      tampungRoman.push(" "+angka[satuan] + " belas")
+      }
+      else if(satuan==1){
+      tampungRoman.push(" "+"sebelas")
+      }
+
+    }
+    // push satuan
+    else if(puluhan==0){
+      tampungRoman.push(angka[satuan])
+    }
+
+
+    return tampungRoman.join("");
 }
 
-in_words(2)
-in_words(23)
-in_words(234)
-in_words(2345)
-in_words(23457)
-in_words(234578)
-in_words(2345782)
-in_words(23457828)
+console.log(in_words(4));
+console.log(in_words(27));
+console.log(in_words(102));
+console.log(in_words(38079));
+console.log(in_words(82102713));
--- number_in_words_es6.js
+++ number_in_words_es6.js
@@ -1,162 +1,88 @@
 console.log("ES6 compiler is starting");
-let toWords = angkaToWord => {
-  let result = []
-  let satuanAngka = ["satu", "dua", "tiga","empat",'lima','enam','tujuh','delapan','sembilan'];
-  let angkaSatuan = [1,2,3,4,5,6,7,8,9]
-  let angkaInWordPuluhan = ['sepuluh','sebelas','belas','puluh']
-  let angkaInWordRatusan = ['seratus','ratus']
-  let angkaInWordRibuan = ['seribu','ribu']
+let toWords = n => {
+  let tampungRoman = []
+  let angka = ["","satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"]
 
+  let ratusJuta = Math.floor((n%1000000000)/100000000)
+  let puluhJuta = Math.floor((n%100000000)/10000000)
+  let jutaan = Math.floor((n%10000000)/1000000)
+  let ratusRibu = Math.floor((n%1000000)/100000)
+  let puluhRibuan = Math.floor((n%100000)/10000)
+  let ribuan = Math.floor((n%10000)/1000)
+  let ratusan = Math.floor((n%1000)/100)
+  let puluhan = Math.floor((n%100)/10)
+  let satuan = Math.floor((n%10)/1)
 
-  let ratusJuta = Math.floor((angkaToWord%1000000000)/100000000)
-  let puluhJuta = Math.floor((angkaToWord%100000000)/10000000)
-  let jutaan = Math.floor((angkaToWord%10000000)/1000000)
-  let ratusRibu = Math.floor((angkaToWord%1000000)/100000)
-  let puluhRibuan = Math.floor((angkaToWord%100000)/10000)
-  let ribuan = Math.floor((angkaToWord%10000)/1000)
-  let ratusan = Math.floor((angkaToWord%1000)/100)
-  let puluhan = Math.floor((angkaToWord%100)/10)
-  let satuan = Math.floor((angkaToWord%10)/1)
-
-  console.log(puluhJuta+" "+jutaan+" "+ratusRibu+" "+puluhRibuan+" "+ribuan+" "+ratusan+" "+puluhan+" "+satuan);
-
-
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if( i === puluhJuta)
-    {
-      if(puluhJuta===0)break
-      result.push(satuanAngka[i-1] + " puluh")
+  // push puluhan
+  if(puluhJuta>1){
+    tampungRoman.push(" " +angka[puluhJuta] + " puluh ")
+    if(jutaan>0){
+    tampungRoman.push(" " +angka[jutaan]+ " juta ")
     }
   }
+  else if(puluhJuta==1){
+    if(jutaan>1){
+    tampungRoman.push(" "+angka[jutaan] + " belas juta ")
+    }
+    else if(jutaan==1){
+    tampungRoman.push(" "+"sebelas juta ")
+    }
 
+  }
+  // push puluhRibuan
+  else if(puluhRibuan==0){
+    tampungRoman.push(angka[jutaan])
+  }
 
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if( i === jutaan)
-    {
-      if(jutaan===0)break
-      result.push(satuanAngka[i-1] + " juta")
+  // push ratusRibuan
+  if(ratusRibu==1){
+    tampungRoman.push("seratus ")
+  }
+  else if(ratusRibu>0){
+  tampungRoman.push(angka[ratusRibu] + " ratus ")
+  }
+
+  // push puluhRibuan
+  if(puluhRibuan>0){
+  tampungRoman.push(angka[puluhRibuan] + " puluh ")
+  }
+  // push ribuan
+    if(ribuan>0){
+    tampungRoman.push(angka[ribuan] + " ribu")
+    }
+  // push ratusan
+  if(ratusan==1){
+    tampungRoman.push("seratus ")
+  }
+
+  else if(ratusan>0){
+    tampungRoman.push(" " +angka[ratusan] + " ratus")
+  }
+  // push puluhan
+  if(puluhan>1){
+    tampungRoman.push(" " +angka[puluhan] + " puluh")
+    if(satuan>0){
+    tampungRoman.push(" " +angka[satuan])
     }
   }
-
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if( i === ratusRibu)
-    {
-      if(ratusRibu===0)break
-      result.push(satuanAngka[i-1] + " ratus ribu")
+  else if(puluhan==1){
+    if(satuan>1){
+    tampungRoman.push(" "+angka[satuan] + " belas")
     }
-  }
+    else if(satuan==1){
+    tampungRoman.push(" "+"sebelas")
+    }
 
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if( i === puluhRibuan)
-    {
-      if(puluhRibuan===0)break
-      result.push(satuanAngka[i-1] + " puluh")
-    }
   }
+  // push satuan
+  else if(puluhan==0){
+    tampungRoman.push(angka[satuan])
+  }
+  return tampungRoman.join("");
+}
 
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if( i === ribuan)
-    {
-      if(ribuan===0)break
-      result.push(satuanAngka[i-1] + " ribu")
-    }
-  }
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if(i === ratusan)
-    {
-      if(ratusan===0)break
-      result.push(satuanAngka[i-1]+ " ratus")
-    }
-  }
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if(angkaToWord > 10 && angkaToWord <= 19)
-    {
-      if(angkaToWord===11)
-      {
-      result.push("sebelas")
-      break
-    }else if(angkaToWord > 11)
-    {
-      if(angkaToWord === 12){
-      result.push(satuanAngka[i+1]+" belas")
-      break
-    } else if(angkaToWord === 13){
-      result.push(satuanAngka[i+2]+" belas")
-      break
-    }else if(angkaToWord === 14){
-      result.push(satuanAngka[i+3]+" belas")
-      break
-    }
-    else if(angkaToWord === 15){
-      result.push(satuanAngka[i+4]+" belas")
-      break
-    }else if(angkaToWord === 16){
-      result.push(satuanAngka[i+5]+" belas")
-      break
-    }else if(angkaToWord === 17){
-      result.push(satuanAngka[i+6]+" belas")
-      break
-    }else if(angkaToWord === 18){
-      result.push(satuanAngka[i+7]+" belas")
-      break
-    }else if(angkaToWord === 19){
-      result.push(satuanAngka[i+8]+" belas")
-      break
-    }
-    }
-      if(i===satuan)
-      {
-        if(satuan===0)break
-        result.push(satuanAngka[i-1])
-     }
-    }else if(angkaToWord>=20){
-    if(i===puluhan)
-    {
-      if(puluhan===0)break
-       result.push(satuanAngka[i-1]+ " puluh")
-    }
-  }
-  }
-
-  for(let i=0;i<satuanAngka.length+1;i++){
-    if(angkaToWord > 10 && angkaToWord <= 19){
-      if(i===puluhan)
-      {
-        if(puluhan===0)break
-         //result.push(satuanAngka[i-1]+ " puluh")
-      }
-    }else if(angkaToWord >= 20){
-     if(i===satuan)
-     {
-       if(satuan===0)break
-       result.push(satuanAngka[i-1])
-    }
-  }
-  else if(angkaToWord >= 1 && angkaToWord < 10){
-   if(i===satuan)
-   {
-     if(satuan===0)break
-     result.push(satuanAngka[i-1])
-  }
-  }else if(angkaToWord === 10)
-  {
-    result.push("sepuluh")
-    break
-  }
-  }
-  console.log(result.join(' '));
-  }
-
-  toWords(2)
-  toWords(23)
-  toWords(234)
-  toWords(2345)
-  toWords(23457)
-  toWords(234578)
-  toWords(2345782)
-  toWords(23457828)
+console.log(toWords(4));
+console.log(toWords(27));
+console.log(toWords(102));
+console.log(toWords(38079));
+console.log(toWords(82102713));

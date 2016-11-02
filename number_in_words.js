2419cfb6168acaab822cb8f2ef4d37000c40e400
Your Name <name@example.com>
2016-11-02 22:06:56 +0700

    1-9Triliun Complite Multi Input

--- number_in_words.js
+++ number_in_words.js
@@ -3,14 +3,17 @@ var j = 0;
 
 function in_words(angka) {
     var strAngka = String(angka);
-    if(strAngka.length >13){
-      return "Diluar Batas 'Max 9 Triliun'"
-    }else if (angka <= 0) {
+    if (strAngka.length > 13) {
+        return "Diluar Batas 'Max 9 Triliun'"
+    } else if (angka <= 0) {
         kalimat = kalimat.replace("Satu Ribu", "Seribu");
         kalimat = kalimat.replace("Juta Ribu", "Juta");
         kalimat = kalimat.replace("Milyar Juta", "Milyar");
         kalimat = kalimat.replace("Triliun Milyar", "Triliun");
-        return kalimat;
+        var hasil = kalimat;
+        j = 0;
+        kalimat = "";
+        return hasil;
     } else {
         var arrAngka = strAngka.split("")
         var kata = ['', 'Satu ', 'Dua ', 'Tiga ', 'Empat ', 'Lima ', 'Enam ', 'Tujuh ', 'Delapan ', 'Sembilan '];
@@ -42,12 +45,12 @@ function in_words(angka) {
         //Ratusan
         if (value3 == "1") {
             ratusan = "Seratus "
-        } else if (value3 != "1" && panjang != "2" && panjang != "1" && value3 !="0") {
+        } else if (value3 != "1" && panjang != "2" && panjang != "1" && value3 != "0") {
             ratusan = kata[value3] + " Ratus "
         }
 
         kalimat = (ratusan + puluhan + satuanBelasan + tingkat[j]) + kalimat
-        j ++;
+        j++;
         var str = String(angka)
         var arrSlice = str.substring(0, str.length - 3)
         var num = Number(arrSlice)
@@ -55,5 +58,12 @@ function in_words(angka) {
     }
 }
 
-//console.log(in_words(1))
+console.log(in_words(1))
+console.log(in_words(99))
+console.log(in_words(100))
+console.log(in_words(1000))
+console.log(in_words(1000003))
 console.log(in_words(1000000))
+console.log(in_words(1000000009))
+console.log(in_words(1000000000))
+console.log(in_words(1001009000000))
